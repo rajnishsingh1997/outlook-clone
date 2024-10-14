6837a31d318436a56e6f967242da0138867d1025
rajkumar1308 <103402077+rajkumar1308@users.noreply.github.com>
2024-10-14 10:27:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Emailbodyprev from './components/Emailbodyprev';
 import UserData from './components/userdata';
 
 const API = "https://flipkart-email-mock.now.sh/";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [selectedCardId, setSelectedCardId] = useState(null);
+  const [selectedCardDetails, setSelectedCardDetails] = useState(null);
 
   const fetchUsers = async (url) => {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      // Accessing the `list` property where the emails are stored
       if (data && data.list) {
         setUsers(data.list);
       }
@@ -24,10 +26,16 @@ function App() {
     fetchUsers(API);
   }, []);
 
+  const handleCardClick = (id) => {
+    setSelectedCardId(id);
+    const selectedCard = users.find(item => item.id === id);
+    setSelectedCardDetails(selectedCard);
+  };
+
   return (
     <>
       <section>
-      <div className="header-body">
+        <div className="header-body">
           <ul>
             <li>Filter By </li>
             <li onClick={() => setFilter('unread')}>Unread</li>
@@ -35,13 +43,14 @@ function App() {
             <li onClick={() => setFilter('favorites')}>Favorites</li>
           </ul>
         </div>
-        <div className="banner full-body">
-          <div className="subject-body">
-            {/* Pass the `users` state to `UserData` component */}
-            <UserData users={users} />
+        <div className="main-container">
+          <div className={`email-list ${selectedCardId ? 'shrink' : ''}`}>
+            <UserData users={users} onCardClick={handleCardClick} />
+          </div>
+          <div className={`email-body ${selectedCardId ? 'expanded' : ''}`}>
+            {selectedCardId && <Emailbodyprev details={selectedCardDetails} />}
           </div>
         </div>
-       
       </section>
     </>
   );
